d00b77b7daceba8d812ac462abf372799af09e12
Chris Gearhart <cgearhart@gmail.com>
2017-08-30 16:36:35 -0700

    Add alternate link to DIC paper

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -86,6 +86,7 @@ class SelectorDIC(ModelSelector):
     Biem, Alain. "A model selection criterion for classification: Application to hmm topology optimization."
     Document Analysis and Recognition, 2003. Proceedings. Seventh International Conference on. IEEE, 2003.
     http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.58.6208&rep=rep1&type=pdf
+    https://pdfs.semanticscholar.org/ed3d/7c4a5f607201f3848d4c02dd9ba17c791fc2.pdf
     DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
     '''
 
